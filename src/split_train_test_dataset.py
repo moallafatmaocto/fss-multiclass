d59cd666328360e46e55dd7e26949294af32a559
Maria Mokbel <mmokbel@octo.com>
2020-09-02 11:13:05 +0200

    refactoring of split train test file

--- src/split_train_test_dataset.py
+++ src/split_train_test_dataset.py
@@ -1,42 +1,31 @@
 import os, shutil
 import random
-
-train_category_number = 760
-test_category_number = 240
+from typing import Tuple, List
 
 
-def get_random_N_classes(class_list, n):
+def get_random_n_classes(class_list: List, n: int) -> List:
     classes = list(range(0, len(class_list)))
     print('total number of initial classes', len(classes))
     chosen_classes = random.sample(classes, n)
     return chosen_classes
 
 
-# Set up directory
-data_path = 'data/'
+def get_train_and_test_image_lists(class_list: List, train_class_number: int) -> Tuple[List, List]:
+    print('class_list', class_list)
+    chosen_train_classes = [class_list[idx] for idx in (get_random_n_classes(class_list, train_class_number))]
+    print('Number of train classes', len(chosen_train_classes))
+    chosen_test_classes = [class_name for class_name in class_list if (class_name not in (chosen_train_classes))]
+    print('Number of test classes', len(chosen_test_classes), chosen_test_classes)
 
-if not os.path.exists('../data/train'):
-    os.mkdir('../data/train')
-if not os.path.exists('../data/test'):
-    os.mkdir('../data/test')
-original_data_path = '../data/'
-new_path_train = original_data_path + 'train'
-new_path_test = original_data_path + 'test'
+    return chosen_train_classes, chosen_test_classes
 
-# Determine randomly the dataset
-class_list = os.listdir('../data/')
-print('class_list', class_list)
-class_list.remove('train')
-class_list.remove('test')
-class_list.remove('.DS_Store')
-chosen_train_classes_idx = get_random_N_classes(class_list, train_category_number)
-chosen_train_classes = [class_list[idx] for idx in chosen_train_classes_idx]
-print('Number of train classes', len(chosen_train_classes))
-chosen_test_classes = [class_name for class_name in class_list if
-                       (class_name not in (chosen_train_classes) and class_name != 'test' and class_name != 'train')]
 
-# chosen_test_classes = [ class_name for class_name in [0,1,2,3,4] if class_name not in [0,1]]
-print('Number of test classes', len(chosen_test_classes), chosen_test_classes)
+def create_images_directories(base_path: str, directory: str) -> str:
+    path = base_path + directory
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    return path
 
 
 def move_to_new_dir(classes, original_path, new_path):
@@ -48,5 +37,12 @@ def move_to_new_dir(classes, original_path, new_path):
     print('Moving ended.')
 
 
-move_to_new_dir(chosen_train_classes, original_data_path, new_path_train)
-move_to_new_dir(chosen_test_classes, original_data_path, new_path_test)
+if __name__ == '__main__':
+    base_path = '../data/'
+    train_classes_number = 760
+    class_list = os.listdir(base_path)
+    train_classes, test_classes = get_train_and_test_image_lists(class_list, train_classes_number)
+    train_path = create_images_directories(base_path, 'train')
+    test_path = create_images_directories(base_path, 'test')
+    move_to_new_dir(train_classes, base_path, train_path)
+    move_to_new_dir(test_classes, base_path, test_path)
